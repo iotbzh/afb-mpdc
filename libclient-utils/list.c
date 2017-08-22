5143e6cc31cfe981527f04c7a54038407ab0bdde
Sebastien Douheret <sebastien.douheret@iot.bzh>
2017-08-22 09:49:59 +0200

    Fix invalid arguments when calling afb_req_fail_f
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- libclient-utils/list.c
+++ libclient-utils/list.c
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * References:
  *  Utilities rebuilt from Media Daemon Command Line Interface
  *  https://github.com/MusicPlayerDaemon/libmpdclient
@@ -30,15 +30,15 @@
 #include "libclient-utils.h"
 
 STATIC json_object* GetEntityList(afb_req request,mpdcHandleT *mpdcHandle, const char *path, enum mpd_entity_type filter_type) {
-    
+
     json_object *elemJ, *responseJ= json_object_new_array();
 
     int done = mpd_send_list_meta(mpdcHandle->mpd,path);
     if (!done) {
         afb_req_fail_f(request, "MPDC:ListDirFile fail to send_list_meta Daemon=%s name=%s", mpdcHandle->label, path);
         goto OnErrorExit;
-    }                    
-    
+    }
+
     struct mpd_entity *entity;
     while ((entity = mpd_recv_entity(mpdcHandle->mpd)) != NULL) {
         const struct mpd_directory *dir;
@@ -72,61 +72,61 @@ STATIC json_object* GetEntityList(afb_req request,mpdcHandleT *mpdcHandle, const
         }
         if (type !=  MPD_ENTITY_TYPE_SONG)  mpd_entity_free(entity);
     }
-    
+
     if (!mpd_response_finish(mpdcHandle->mpd)) {
         miscPostError(request,"MPDC:GetEntityList", mpdcHandle);
         goto OnErrorExit;
     }
-    
+
     return responseJ;
 
-OnErrorExit:    
+OnErrorExit:
     json_object_put(responseJ);
     return NULL;
-    
+
 }
 
 
 PUBLIC json_object* ListDirSong (afb_req request, mpdcHandleT *mpdcHandle, json_object *pathsJ) {
     json_object *responseJ, *pathJ, *entityJ;
     const char *path;
-       
+
     switch (json_object_get_type (pathsJ)) {
-        
+
         case json_type_array:
-            
+
             responseJ= json_object_new_array();
-            
+
             for (int idx=0; idx < json_object_array_length(pathsJ); idx ++) {
                 pathJ = json_object_array_get_idx(pathsJ, idx);
                 path= json_object_get_string(pathJ);
                 if (!pathJ) {
                     afb_req_fail (request, "MPDC:ListDirSong path not an array of string path=%s", path);
-                    goto OnErrorExit;                    
+                    goto OnErrorExit;
                 }
                 entityJ=GetEntityList (request, mpdcHandle, charset_to_utf8(path), MPD_ENTITY_TYPE_UNKNOWN);
-                if (entityJ) json_object_array_add(responseJ, entityJ); 
+                if (entityJ) json_object_array_add(responseJ, entityJ);
             }
-       
+
         break;
-        
+
         case json_type_string:
             path= json_object_get_string(pathsJ);
             entityJ=GetEntityList (request, mpdcHandle, charset_to_utf8(path), MPD_ENTITY_TYPE_UNKNOWN);
             break;
-            
+
         case json_type_object:
         case json_type_null:
             responseJ=GetEntityList (request, mpdcHandle, NULL, MPD_ENTITY_TYPE_UNKNOWN);
             break;
-            
+
         default:
-            afb_req_fail_f(request, "MPDC:ListDirSong path should be a string a an array of string path=%s", json_object_get_string(pathsJ));
+            afb_req_fail_f(request, "MPDC:ListDirSong", "Path should be a string a an array of string path=%s", json_object_get_string(pathsJ));
             goto OnErrorExit;
     }
 
     return responseJ;
-    
+
 OnErrorExit:
     return NULL;
 }
@@ -147,22 +147,22 @@ PUBLIC json_object *ListPlayList (afb_req request, mpdcHandleT *mpdcHandle, cons
     } else { // no name return current queue list
         mpd_send_list_queue_meta(mpdcHandle->mpd);
     }
-    
+
     struct mpd_song *song;
     while ((song = mpd_recv_song(mpdcHandle->mpd)) != NULL) {
         json_object *songJ = CtlPlayCurrentSong(song);
         json_object_array_add(responseJ, songJ);
         count ++;
     }
- 
+
     if (count == 0) {
         if (!name) name="Current/Active";
         afb_req_fail(request, "MPDC:PlaylistPrint List Empty playlist=%s", name);
-        goto OnErrorExit;        
+        goto OnErrorExit;
     }
-    
+
     return responseJ;
-    
+
 OnErrorExit:
     json_object_put(responseJ);
     return NULL;
